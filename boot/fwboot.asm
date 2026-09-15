17643ece8c21d04e47e3729da304b1636d07b19e
Mayank Pathak <kapnpetrov1321@gmail.com>
2026-09-15 20:05:13 +0530

    [FEATURE]: Add ECALLs.

--- boot/fwboot.asm
+++ boot/fwboot.asm
@@ -33,6 +33,7 @@ _start:
     ;Set up the stack top from linker
     la sp, _stack_top
     la t0, TrapHandler
+    andi t0, t0, -4
     ;On machine mode MTVEC holds exception call address
     csrw mtvec, t0
 
@@ -40,9 +41,56 @@ _start:
 	call MainStartup
 hangboot: j hangboot
 
+.align 4
 TrapHandler:
-    ;Just write a marker now so we know while dbg
-	li t0, 0x20000000
-	li t1, 0xDEADBEEF
-	sw t1, 0(t0)
-	j TrapHandler
+	addi sp, sp, -64
+	sw ra,  0(sp)
+	sw t0,  4(sp)
+	sw t1,  8(sp)
+	sw t2, 12(sp)
+	sw a0, 16(sp)
+	sw a1, 20(sp)
+	sw a2, 24(sp)
+	sw a3, 28(sp)
+	sw a4, 32(sp)
+	sw a5, 36(sp)
+	sw a6, 40(sp)
+	sw a7, 44(sp)
+	sw t3, 48(sp)
+	sw t4, 52(sp)
+	sw t5, 56(sp)
+	sw t6, 60(sp)
+
+	csrr t0, mcause
+	;ecall
+	li t1, 0xb
+	bne t0, t1, .skip_ecall
+
+	call MainHandleTrap
+
+	;advance by 4 if ecall
+	csrr t2, mepc
+	addi t2, t2, 4
+	csrw mepc, t2
+
+.skip_ecall:
+	;not ecall, just return
+	lw ra,  0(sp)
+	lw t0,  4(sp)
+	lw t1,  8(sp)
+	lw t2, 12(sp)
+	lw a0, 16(sp)
+	lw a1, 20(sp)
+	lw a2, 24(sp)
+	lw a3, 28(sp)
+	lw a4, 32(sp)
+	lw a5, 36(sp)
+	lw a6, 40(sp)
+	lw a7, 44(sp)
+	lw t3, 48(sp)
+	lw t4, 52(sp)
+	lw t5, 56(sp)
+	lw t6, 60(sp)
+	addi sp, sp, 64
+
+	mret
--- dos/main.asm
+++ dos/main.asm
@@ -8,6 +8,7 @@
 
 .section .text
 .global MainStartup
+.global MainHandleTrap
 
 ;
 ; FUNCTION DESCRIPTION
@@ -26,3 +27,32 @@ MainStartup:
 	call CmdInit
 
 main_loop: j main_loop
+
+;
+; FUNCTION DESCRIPTION
+;
+; This handles ECALL dispatching and invoked only by trap
+; handler.
+;
+;
+MainHandleTrap:
+	addi sp, sp, -16
+	sw ra, 12(sp)
+
+	;ecall 1
+	li t0, 1
+	beq a0, t0, .ex_write_char
+
+	lw ra, 12(sp)
+	addi sp, sp, 16
+	ret
+
+;ECALL 1 - WriteCharacter
+;a1 - arg1
+.ex_write_char:
+	mv a0, a1
+	call WriteCharacter
+
+	lw ra, 12(sp)
+	addi sp, sp, 16
+	ret
